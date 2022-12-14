9fc4c8d65bd2edc0c6036bb91cd7b5131370c695
alidogangullu <alidogangullu@outlook.com>
2022-12-14 12:06:27 +0300

    Initial commit

---
+++ lib/firebase_options.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
+
+const firebaseOptions = FirebaseOptions(
+  appId: '1:1083970971819:web:61acfab0aade546fe5efee',
+  apiKey: 'AIzaSyB-kvN9Ldpzmk343X0h95tD9yxzrC9Lelg',
+  projectId: 'restaurantapp-2a43d',
+  messagingSenderId: '1083970971819',
+  authDomain: 'restaurantapp-2a43d.firebaseapp.com',
+  storageBucket: 'restaurantapp-2a43d.appspot.com',
+  measurementId: 'G-DT51MD11SE',
+);
